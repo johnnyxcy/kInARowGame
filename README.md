057befd661752eec14afec0c48212483836a6756
JohnniaX <johnny.xcy1997@gmail.com>
2018-12-28 18:17:33 -0800

    Update my work

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # kInARowGame
+
+A simple k-in-a-row game clinet which is designed for players to compete their k-in-a-row algorithms.
+
+Thanks for *S. Tanimoto* for providing `TimedGameMaster.py`
+
+Thanks for *Y Wang* and *L Yang* for provding their algorithms for competing.
+
+## Usage
+
+Run either `TimedGameMaster.py --if_html --time_per_move --turn_limit` for a timed game master
+
+Or simply run `GameMaster.py` for a simple game.
---
+++ gameToHTML.py
@@ -0,0 +1,59 @@
+'''gameToHTML.py
+
+'''
+from pathlib import Path
+
+f = None
+def startHTML(nickName1, nickName2, gameType, round=1):
+    if not Path('./results').exists():
+        Path('./results').mkdir()
+    # Create a filename.
+    fn = Path('./results/'+clean(nickName1)+'-vs-'+clean(nickName2)+'-in-'+clean(gameType)+'-round-'+str(round)+'.html')
+    while fn.exists():
+        round = int(fn.as_posix().split('.')[0][-1])
+        fn = Path('./results/'+clean(nickName1)+'-vs-'+clean(nickName2)+'-in-'+clean(gameType)+'-round-'+str(round+1)+'.html')
+    global F
+    try: F = open(fn, "w")
+    except:
+        print("Could not open the file "+fn+" for the game's HTML page.")
+        return
+    F.write('''
+<html><head><title>K-in-a-Row game</title></head>
+<body>
+<h1>Game Report: ''')
+    F.write(nickName1 + ' versus ' + nickName2 + ' in ' +gameType + ', round '+str(round))
+
+    F.write(''' </h1>
+''')
+
+def reportResult(result):
+    F.write("<h2>"+result+"</h2>\n")
+
+def endHTML():
+    F.write("</body></html>\n")
+    F.close()
+
+    
+def clean(name):
+    import re
+    new_name = re.sub(' ', '-', name)
+    new_name = re.sub('[^a-zA-Z10-9\\-]', '', new_name)
+    return new_name
+
+def stateToHTML(state, finished=False):
+    board, who = state
+    html = '''<table>
+'''
+    for row in board:
+        html += "<tr>"
+        for col in row:
+            img = "gray32.png"
+            if col=='X': img = "X32.png"
+            elif col=='O': img = "O32.png"
+            elif col=="-": img = "black32.png"
+            html += "<td><img src=" + img + "></td>"
+        html += "</tr>\n"
+    html += "</table><br>\n"
+    if not finished: html += "<h3>"+who+" to move.</h3>\n"
+    F.write(html)
+    
